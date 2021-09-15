45b948fb1882f4d2a5358de00de648d5db0dd97b
Tomas I. Opazo <toopazo@protonmail.com>
2021-09-15 19:04:47 -0400

    mejoras al live_esc

--- kde_uas85uvc/kdecan_interface.py
+++ kde_uas85uvc/kdecan_interface.py
@@ -1,236 +1,14 @@
 #!/usr/bin/python3
-import os
 
+import os
 import can
-import time
 import numpy as np
-import signal
-import sys
 import datetime
 import subprocess
 from kdecan_parseMsg import ParseMsg
-from toopazo_tools.file_folder import FileFolderTools as FFTools
 
 
-class KdeCanLive:
-    def __init__(self, folder):
-        # assert isinstance(kdecanapi, KdeCanAPI)
-        self.kdecanapi = KdeCanAPI()
-
-        self.log_filename = None
-        self.log_fd = None
-        # log_141_2020-12-22-13-41-26.ulg
-        self.log_filename_separator = '_'
-        self.log_filename_logstr = 'log'
-        self.log_filename_extension = '.kdecan'
-
-        # folder for logs
-        self.log_folder = folder
-        self.logs_in_folder = self.find_logs_in_folder()
-
-        self.time0 = time.time()
-        signal.signal(signal.SIGINT, self.signal_handler)
-
-    def find_logs_in_folder(self):
-        file_lognum_arr = []
-        lognum_arr = []
-        logdate_arr = []
-        logname_arr = []
-
-        farr = FFTools.get_file_arr(
-            self.log_folder, extension=self.log_filename_extension)
-        for file in farr:
-            [head, tail] = FFTools.get_file_split(file)
-            _ = head
-            logname = tail
-            res = self.parse_log_filename(logname)
-            if res is not None:
-                [logstr, lognum, logdate] = res
-                _ = logstr
-                file_lognum_arr.append(lognum)
-                print('[find_logs_in_folder] lognum {}, logdate  {}, logname {}'
-                      .format(lognum, logdate, logname))
-                lognum_arr.append(lognum)
-                logdate_arr.append(logdate)
-                logname_arr.append(logname)
-
-        logs_in_folder = {'folder': self.log_folder, 'lognum': lognum_arr,
-                          'logdate': logdate_arr, 'logname': logname_arr}
-        return logs_in_folder
-
-    def parse_log_filename(self, filename):
-        if self.log_filename_extension in filename:
-            # log_141_2020-12-22-13-41-26.ulg
-            filename = filename.replace(self.log_filename_extension, '')
-            farr = filename.split(self.log_filename_separator)
-
-            print('[parse_log_filename] filename {}'.format(filename))
-            print('[parse_log_filename] farr {}'.format(farr))
-            logstr = farr[0] == self.log_filename_logstr
-            lognum = int(farr[1])
-            logdate = datetime.datetime.strptime(farr[2], '%Y-%m-%d-%H-%M-%S')
-
-            if len(farr) != 3:
-                return None
-            else:
-                # print([logstr, lognum, logdate])
-                return [logstr, lognum, logdate]
-        else:
-            return None
-
-    def new_log_filename(self):
-        self.logs_in_folder = self.find_logs_in_folder()
-        if len(self.logs_in_folder['lognum']) > 0:
-            prev_lognum = np.max(self.logs_in_folder['lognum'])
-        else:
-            prev_lognum = 0
-
-        dtnow = datetime.datetime.now()
-        logstr = self.log_filename_logstr
-        lognum = str(prev_lognum + 1)
-        logdate = dtnow.strftime("%Y-%m-%d-%H-%M-%S")
-
-        separator = self.log_filename_separator
-        extension = self.log_filename_extension
-        fnarr = [
-            logstr, separator, lognum, separator, logdate, extension
-        ]
-        log_filename = ''.join(fnarr)
-        return log_filename
-
-    def close_it_all(self):
-        # Close file
-        if self.log_fd is None:
-            pass
-        else:
-            self.log_fd.close()
-
-        # close can bus
-        self.kdecanapi.kdecan_bus.shutdown()
-
-    def signal_handler(self, _signal, _frame):
-        print('[signal_handler] calling self.kdecan_bus.shutdown() ..')
-        print('[signal_handler] signal {}'.format(_signal))
-        print('[signal_handler] frame  {}'.format(_frame))
-        self.close_it_all()
-        sys.exit(0)
-
-    def busy_waiting(self, t0, period, dt):
-        telap = time.time() - t0
-        ncycles = int(telap / period)
-        tf = ncycles*period + period
-        # print("[busy_waiting] t0 %s" % t0)
-        # print("[busy_waiting] tf %s" % tf)
-
-        # Busy waiting while current time "ti" is less than final time "tf"
-        telap = time.time() - t0
-        while telap < tf:
-            telap = time.time() - t0
-            # print("[busy_waiting] telap %s" % telap)
-            time.sleep(dt)
-
-    def test_pwm(self):
-
-        # resp = self.set_restartesc(targetid, kdecan_recv_timeout)
-        # arg = "[test_pwm] restartesc %mysched " % resp
-        # print(arg)
-        # time.sleep(20)
-
-        # esc_arr = [11, 12, 13, 14, 15, 16, 17, 18]
-        esc_arr = [11, 16]
-
-        # Spin motors
-        # nesc = len(esc_arr)
-        # throttle = 1300
-        # throttle_arr = throttle * np.ones(nesc)
-        # self.set_pwm_esc_arr(esc_arr, throttle_arr, kdecan_recv_timeout)
-
-        # Write first line of csv file
-        arg = "time mysched, escid, " \
-              "voltage V, current A, angVel rpm, temp degC, warning, " \
-              "inthtl us, outthtl perc"
-        print(arg)
-        self.log_fd.write(arg + "\r\n")
-
-        # Perform test
-        niter = 0
-        sampling_delay = 0.1
-        # while niter <= 10:
-        while True:
-            niter = niter + 1
-
-            resp_arr = self.kdecanapi.get_data_esc_arr(esc_arr)
-
-            # for each targetid
-            for resp in resp_arr:
-                print(resp)
-                [telap, targetid,
-                 voltage, current, rpm, temp, warning,
-                 inthrottle, outthrottle] = resp
-
-                arg = "%s, %s, %04.2f, %s, %07.2f, %s, %s, %s, %s " % \
-                      (telap, targetid,
-                       voltage, current, rpm, temp, warning,
-                       inthrottle, outthrottle)
-                print(arg)
-                self.log_fd.write(arg + "\r\n")
-
-            # throttle = 1100 + niter * 70
-            # throttle_arr = throttle*np.ones(nesc)
-            # self.set_pwm_esc_arr(esc_arr, throttle_arr, kdecan_recv_timeout)
-
-            self.busy_waiting(self.time0, 0.1, 0.01)
-
-        # Terminate, if out of while loop
-        # self.close_it_all()
-
-    def live_data(self, esc_arr, sampling_period):
-        # Open file
-        self.log_filename = self.new_log_filename()
-        self.log_fd = open(self.log_filename, "w")
-
-        # Write first line of csv file
-        log_header = "time s, escid, " \
-                     "voltage V, current A, angVel rpm, temp degC, warning, " \
-                     "inthtl us, outthtl perc"
-        print(log_header)
-        self.log_fd.write(log_header + "\r\n")
-
-        # Perform test
-        # niter = 0
-        # while niter <= 10:
-        #     niter = niter + 1
-        while True:
-            # get data
-            resp_arr = self.kdecanapi.get_data_esc_arr(esc_arr)
-
-            # for each targetid, save data to file
-            for resp in resp_arr:
-                [telap, targetid,
-                 voltage, current, rpm, temp, warning,
-                 inthrottle, outthrottle] = resp
-
-                log_data = "%s, %s, %04.2f, %s, %07.2f, %s, %s, %s, %s " % \
-                           (telap, targetid,
-                            voltage, current, rpm, temp, warning,
-                            inthrottle, outthrottle)
-                # log_data = "%s s, %s escid, " \
-                #       "%04.2f V, %s A, %07.2f rpm, %s degC, %s, " \
-                #       "%s us, %s perc " % \
-                #       (telap, targetid,
-                #        voltage, current, rpm, temp, warning,
-                #        inthrottle, outthrottle)
-                print(log_data)
-                self.log_fd.write(log_data + "\r\n")
-
-            # busy wait for next loop
-            self.busy_waiting(self.time0, sampling_period, sampling_period / 8)
-
-        # # Terminate, if out of while loop
-        # self.close_it_all()
-
-
-class KdeCanAPI:
+class KdeCanIface:
 
     def __init__(self):
         if self.check_can0_status():
@@ -427,69 +205,69 @@ class KdeCanAPI:
         for i in range(0, self.kdecan_max_num_tries):
             self.kdecan_bus.send(message, timeout=self.kdecan_send_timeout)
             msg = self.kdecan_bus.recv(timeout=self.kdecan_recv_timeout)
-            if KdeCanAPI.check_recv_msg(msg, targetid, objctadd):
+            if KdeCanIface.check_recv_msg(msg, targetid, objctadd):
                 return ParseMsg.parse_recv_msg(msg, objctadd)
         return None
 
     def get_esc_info(self, targetid):
         objctadd = 0x00
         data_arr = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        message = KdeCanAPI.assemble_can_message(targetid, objctadd, data_arr)
+        message = KdeCanIface.assemble_can_message(targetid, objctadd, data_arr)
         return self.robust_msg_transmition(message, targetid, objctadd)
 
     def get_voltage(self, targetid):
         objctadd = 0x02
         data_arr = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        message = KdeCanAPI.assemble_can_message(targetid, objctadd, data_arr)
+        message = KdeCanIface.assemble_can_message(targetid, objctadd, data_arr)
         return self.robust_msg_transmition(message, targetid, objctadd)
 
     def get_current(self, targetid):
         objctadd = 0x03
         data_arr = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        message = KdeCanAPI.assemble_can_message(targetid, objctadd, data_arr)
+        message = KdeCanIface.assemble_can_message(targetid, objctadd, data_arr)
         return self.robust_msg_transmition(message, targetid, objctadd)
 
     def get_rpm(self, targetid):
         objctadd = 0x04
         data_arr = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        message = KdeCanAPI.assemble_can_message(targetid, objctadd, data_arr)
+        message = KdeCanIface.assemble_can_message(targetid, objctadd, data_arr)
         return self.robust_msg_transmition(message, targetid, objctadd)
 
     def get_temperature(self, targetid):
         objctadd = 0x05
         data_arr = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        message = KdeCanAPI.assemble_can_message(targetid, objctadd, data_arr)
+        message = KdeCanIface.assemble_can_message(targetid, objctadd, data_arr)
         return self.robust_msg_transmition(message, targetid, objctadd)
 
     def get_inputthrottle(self, targetid):
         objctadd = 0x06
         data_arr = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        message = KdeCanAPI.assemble_can_message(targetid, objctadd, data_arr)
+        message = KdeCanIface.assemble_can_message(targetid, objctadd, data_arr)
         return self.robust_msg_transmition(message, targetid, objctadd)
 
     def get_outputthrottle(self, targetid):
         objctadd = 0x07
         data_arr = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        message = KdeCanAPI.assemble_can_message(targetid, objctadd, data_arr)
+        message = KdeCanIface.assemble_can_message(targetid, objctadd, data_arr)
         return self.robust_msg_transmition(message, targetid, objctadd)
 
     def get_mcuid(self, targetid):
         objctadd = 0x08
         data_arr = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        message = KdeCanAPI.assemble_can_message(targetid, objctadd, data_arr)
+        message = KdeCanIface.assemble_can_message(targetid, objctadd, data_arr)
         return self.robust_msg_transmition(message, targetid, objctadd)
 
     def get_vcrtw(self, targetid):
         objctadd = 0x0B     # 0x0B = 11
         data_arr = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        message = KdeCanAPI.assemble_can_message(targetid, objctadd, data_arr)
+        message = KdeCanIface.assemble_can_message(targetid, objctadd, data_arr)
         return self.robust_msg_transmition(message, targetid, objctadd)
 
     def set_pwm(self, throttle, targetid):
         objctadd = 0x01
         # data_arr = [0x05, 0xFC, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
         data_arr = ParseMsg.uint16_to_bytearray(np.uint16(throttle))
-        message = KdeCanAPI.assemble_can_message(targetid, objctadd, data_arr)
+        message = KdeCanIface.assemble_can_message(targetid, objctadd, data_arr)
         self.kdecan_bus.send(message, timeout=self.kdecan_send_timeout)
         # msg = self.kdecan_bus.recv(timeout=self.kdecan_recv_timeout)
         # self.check_recv_msg(msg, targetid, objctadd)
@@ -498,7 +276,7 @@ class KdeCanAPI:
     def set_turnoffesc(self, targetid):
         objctadd = 0x20
         data_arr = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        message = KdeCanAPI.assemble_can_message(targetid, objctadd, data_arr)
+        message = KdeCanIface.assemble_can_message(targetid, objctadd, data_arr)
         self.kdecan_bus.send(message, timeout=self.kdecan_send_timeout)
         # msg = self.kdecan_bus.recv(timeout=self.kdecan_recv_timeout)
         # self.check_recv_msg(msg, targetid, objctadd)
@@ -507,7 +285,7 @@ class KdeCanAPI:
     def set_restartesc(self, targetid):
         objctadd = 0x21
         data_arr = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        message = KdeCanAPI.assemble_can_message(targetid, objctadd, data_arr)
+        message = KdeCanIface.assemble_can_message(targetid, objctadd, data_arr)
         self.kdecan_bus.send(message, timeout=self.kdecan_send_timeout)
         # msg = self.kdecan_bus.recv(timeout=self.kdecan_recv_timeout)
         # self.check_recv_msg(msg, targetid, objctadd)
--- kde_uas85uvc/kdecan_main.py
+++ kde_uas85uvc/kdecan_main.py
@@ -1,40 +1,299 @@
 #!/usr/bin/python3
 
-# import can
-# import signal
-# import sys
-# import os
+import sys
+import numpy as np
+import signal
 import time
 import datetime
 from toopazo_tools.file_folder import FileFolderTools as FFTools
-from kdecan_driver import KdeCanAPI, KdeCanLive
-import sys
-import subprocess
+from toopazo_tools.telemetry import TelemetryLogger
+from kdecan_interface import KdeCanIface
+
+
+# class KdeCanLive:
+#     def __init__(self, folder):
+#         # assert isinstance(kdecanapi, KdeCanAPI)
+#         self.kdecanapi = KdeCanAPI()
+#
+#         self.log_filename = None
+#         self.log_fd = None
+#         # log_141_2020-12-22-13-41-26.ulg
+#         self.log_filename_separator = '_'
+#         self.log_filename_logstr = 'log'
+#         self.log_filename_extension = '.kdecan'
+#
+#         # folder for logs
+#         self.log_folder = folder
+#         self.logs_in_folder = self.find_logs_in_folder()
+#
+#         self.time0 = time.time()
+#         signal.signal(signal.SIGINT, self.signal_handler)
+#
+#     def find_logs_in_folder(self):
+#         file_lognum_arr = []
+#         lognum_arr = []
+#         logdate_arr = []
+#         logname_arr = []
+#
+#         farr = FFTools.get_file_arr(
+#             self.log_folder, extension=self.log_filename_extension)
+#         for file in farr:
+#             [head, tail] = FFTools.get_file_split(file)
+#             _ = head
+#             logname = tail
+#             res = self.parse_log_filename(logname)
+#             if res is not None:
+#                 [logstr, lognum, logdate] = res
+#                 _ = logstr
+#                 file_lognum_arr.append(lognum)
+#                 print('[find_logs_in_folder] lognum {}, logdate  {}, logname {}'
+#                       .format(lognum, logdate, logname))
+#                 lognum_arr.append(lognum)
+#                 logdate_arr.append(logdate)
+#                 logname_arr.append(logname)
+#
+#         logs_in_folder = {'folder': self.log_folder, 'lognum': lognum_arr,
+#                           'logdate': logdate_arr, 'logname': logname_arr}
+#         return logs_in_folder
+#
+#     def parse_log_filename(self, filename):
+#         if self.log_filename_extension in filename:
+#             # log_141_2020-12-22-13-41-26.ulg
+#             filename = filename.replace(self.log_filename_extension, '')
+#             farr = filename.split(self.log_filename_separator)
+#
+#             print('[parse_log_filename] filename {}'.format(filename))
+#             print('[parse_log_filename] farr {}'.format(farr))
+#             logstr = farr[0] == self.log_filename_logstr
+#             lognum = int(farr[1])
+#             logdate = datetime.datetime.strptime(farr[2], '%Y-%m-%d-%H-%M-%S')
+#
+#             if len(farr) != 3:
+#                 return None
+#             else:
+#                 # print([logstr, lognum, logdate])
+#                 return [logstr, lognum, logdate]
+#         else:
+#             return None
+#
+#     def new_log_filename(self):
+#         self.logs_in_folder = self.find_logs_in_folder()
+#         if len(self.logs_in_folder['lognum']) > 0:
+#             prev_lognum = np.max(self.logs_in_folder['lognum'])
+#         else:
+#             prev_lognum = 0
+#
+#         dtnow = datetime.datetime.now()
+#         logstr = self.log_filename_logstr
+#         lognum = str(prev_lognum + 1)
+#         logdate = dtnow.strftime("%Y-%m-%d-%H-%M-%S")
+#
+#         separator = self.log_filename_separator
+#         extension = self.log_filename_extension
+#         fnarr = [
+#             logstr, separator, lognum, separator, logdate, extension
+#         ]
+#         log_filename = ''.join(fnarr)
+#         return log_filename
+#
+#     def close_it_all(self):
+#         # Close file
+#         if self.log_fd is None:
+#             pass
+#         else:
+#             self.log_fd.close()
+#
+#         # close can bus
+#         self.kdecanapi.kdecan_bus.shutdown()
+#
+#     def signal_handler(self, _signal, _frame):
+#         print('[signal_handler] calling self.kdecan_bus.shutdown() ..')
+#         print('[signal_handler] signal {}'.format(_signal))
+#         print('[signal_handler] frame  {}'.format(_frame))
+#         self.close_it_all()
+#         sys.exit(0)
+#
+#     @staticmethod
+#     def busy_waiting(t0, period, dt):
+#         telap = time.time() - t0
+#         ncycles = int(telap / period)
+#         tf = ncycles*period + period
+#         # print("[busy_waiting] t0 %s" % t0)
+#         # print("[busy_waiting] tf %s" % tf)
+#
+#         # Busy waiting while current time "ti" is less than final time "tf"
+#         telap = time.time() - t0
+#         while telap < tf:
+#             telap = time.time() - t0
+#             # print("[busy_waiting] telap %s" % telap)
+#             time.sleep(dt)
+#
+#     def test_pwm(self):
+#
+#         # resp = self.set_restartesc(targetid, kdecan_recv_timeout)
+#         # arg = "[test_pwm] restartesc %mysched " % resp
+#         # print(arg)
+#         # time.sleep(20)
+#
+#         # esc_arr = [11, 12, 13, 14, 15, 16, 17, 18]
+#         esc_arr = [11, 16]
+#
+#         # Spin motors
+#         # nesc = len(esc_arr)
+#         # throttle = 1300
+#         # throttle_arr = throttle * np.ones(nesc)
+#         # self.set_pwm_esc_arr(esc_arr, throttle_arr, kdecan_recv_timeout)
+#
+#         # Write first line of csv file
+#         arg = "time mysched, escid, " \
+#               "voltage V, current A, angVel rpm, temp degC, warning, " \
+#               "inthtl us, outthtl perc"
+#         print(arg)
+#         self.log_fd.write(arg + "\r\n")
+#
+#         # Perform test
+#         niter = 0
+#         sampling_delay = 0.1
+#         # while niter <= 10:
+#         while True:
+#             niter = niter + 1
+#
+#             resp_arr = self.kdecanapi.get_data_esc_arr(esc_arr)
+#
+#             # for each targetid
+#             for resp in resp_arr:
+#                 print(resp)
+#                 [telap, targetid,
+#                  voltage, current, rpm, temp, warning,
+#                  inthrottle, outthrottle] = resp
+#
+#                 arg = "%s, %s, %04.2f, %s, %07.2f, %s, %s, %s, %s " % \
+#                       (telap, targetid,
+#                        voltage, current, rpm, temp, warning,
+#                        inthrottle, outthrottle)
+#                 print(arg)
+#                 self.log_fd.write(arg + "\r\n")
+#
+#             # throttle = 1100 + niter * 70
+#             # throttle_arr = throttle*np.ones(nesc)
+#             # self.set_pwm_esc_arr(esc_arr, throttle_arr, kdecan_recv_timeout)
+#
+#             KdeCanLive.busy_waiting(self.time0, 0.1, 0.01)
+#
+#         # Terminate, if out of while loop
+#         # self.close_it_all()
+#
+#     def live_data(self, esc_arr, sampling_period):
+#         # Open file
+#         self.log_filename = self.new_log_filename()
+#         self.log_fd = open(self.log_filename, "w")
+#
+#         # Write first line of csv file
+#         log_header = "time s, escid, " \
+#                      "voltage V, current A, angVel rpm, temp degC, warning, " \
+#                      "inthtl us, outthtl perc"
+#         print(log_header)
+#         self.log_fd.write(log_header + "\r\n")
+#
+#         # Perform test
+#         # niter = 0
+#         # while niter <= 10:
+#         #     niter = niter + 1
+#         while True:
+#             # get data
+#             resp_arr = self.kdecanapi.get_data_esc_arr(esc_arr)
+#
+#             # for each targetid, save data to file
+#             for resp in resp_arr:
+#                 [telap, targetid,
+#                  voltage, current, rpm, temp, warning,
+#                  inthrottle, outthrottle] = resp
+#
+#                 log_data = "%s, %s, %04.2f, %s, %07.2f, %s, %s, %s, %s " % \
+#                            (telap, targetid,
+#                             voltage, current, rpm, temp, warning,
+#                             inthrottle, outthrottle)
+#                 # log_data = "%s s, %s escid, " \
+#                 #       "%04.2f V, %s A, %07.2f rpm, %s degC, %s, " \
+#                 #       "%s us, %s perc " % \
+#                 #       (telap, targetid,
+#                 #        voltage, current, rpm, temp, warning,
+#                 #        inthrottle, outthrottle)
+#                 print(log_data)
+#                 self.log_fd.write(log_data + "\r\n")
+#
+#             # busy wait for next loop
+#             KdeCanLive.busy_waiting(self.time0, sampling_period, sampling_period / 8)
+#
+#         # # Terminate, if out of while loop
+#         # self.close_it_all()
+
+
+class KdeCanIfaceWrapper:
+    def __init__(self):
+        self.kdecan = KdeCanIface()
+        self.esc_arr = list(range(11, 19))
+
+    def get_data(self):
+        # get data
+        resp_arr = self.kdecan.get_data_esc_arr(self.esc_arr)
+
+        # for each targetid, get data
+        log_data_final = ""
+        for resp in resp_arr:
+            [telap, targetid,
+             voltage, current, rpm, temp, warning,
+             inthrottle, outthrottle] = resp
+
+            log_data = "%s, %s, %04.2f, %s, %07.2f, %s, %s, %s, %s " % \
+                       (telap, targetid,
+                        voltage, current, rpm, temp, warning,
+                        inthrottle, outthrottle)
+            # log_data = "%s s, %s escid, " \
+            #       "%04.2f V, %s A, %07.2f rpm, %s degC, %s, " \
+            #       "%s us, %s perc " % \
+            #       (telap, targetid,
+            #        voltage, current, rpm, temp, warning,
+            #        inthrottle, outthrottle)
+            log_data_final = log_data_final + "\r\n" + log_data
+            # self.log_fd.write(log_data + "\r\n")
+        return log_data_final
+
+    def close(self):
+        self.kdecan.kdecan_bus.shutdown()
+
+
+def parse_user_arg(folder):
+    folder = FFTools.full_path(folder)
+    print('target folder {}'.format(folder))
+    if FFTools.is_folder(folder):
+        cwd = FFTools.get_cwd()
+        print('current folder {}'.format(cwd))
+    else:
+        arg = '{} is not a folder'.format(folder)
+        raise RuntimeError(arg)
+    return folder
 
 
 if __name__ == '__main__':
     ufolder = sys.argv[1]
-    ufolder = FFTools.full_path(ufolder)
-    print('target folder {}'.format(ufolder))
-    if FFTools.is_folder(ufolder):
-        cwd = FFTools.get_cwd()
-        print('current folder {}'.format(cwd))
-    else:
-        arg = '{} is not a folder'.format(ufolder)
-        raise RuntimeError(arg)
+    ufolder = parse_user_arg(ufolder)
 
-    # list_files = subprocess.run(["ls", "-l"])
-    # print("returncode: {}".format(list_files.returncode))
-    # print("stdout: {}".format(list_files.stdout))
-    # print("args: {}".format(list_files.args))
-
-    kdecanlive = KdeCanLive(ufolder)
+    # kdecanlive = KdeCanLive(ufolder)
+    kdecanapi = KdeCanIface()
     for uesc in range(11, 19):
-        umsg = kdecanlive.kdecanapi.get_data_esc(uesc)
-        umsg = kdecanlive.kdecanapi.data_esc_to_str(umsg)
+        umsg = kdecanapi.get_data_esc(uesc)
+        umsg = kdecanapi.data_esc_to_str(umsg)
         print(umsg)
 
-    uesc_arr = list(range(11, 19))
-    uperiod = 0.1
-    kdecanlive.live_data(uesc_arr, uperiod)
+    ufolder = ufolder
+    utelemetry_iface = KdeCanIfaceWrapper()
+    utelemetry_ext = ".kdecam"
+    telem_logger = TelemetryLogger(ufolder, utelemetry_iface, utelemetry_ext)
+
+    usampling_period = 0.1
+    ulog_header = "time s, escid, "\
+                  "voltage V, current A, angVel rpm, temp degC, warning, " \
+                  "inthtl us, outthtl perc"
+    telem_logger.live_data(usampling_period, ulog_header)
 
