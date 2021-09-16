23ec5242e2ee46d77380920434794821d8738f3b
Tomas I. Opazo <toopazo@protonmail.com>
2021-09-16 17:19:18 -0400

    adding scritp to ceate venv

--- create_venv.sh
+++ create_venv.sh
@@ -8,5 +8,8 @@ source venv/bin/activate
 # Install libraries
 python -m pip install -U scipy
 python -m pip install -U pyserial
+python -m pip install -U can-utils
+python -m pip install -U net-tools
+python -m pip install -U python-can
 python -m pip install -U toopazo-tools
 
