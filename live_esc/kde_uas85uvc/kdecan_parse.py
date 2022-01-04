5cb22304ae2f1884c5201b95adbdb53e8a8b5302
Tomas I. Opazo <toopazo@protonmail.com>
2022-01-04 15:06:21 -0500

    "automated commit using iacp"

--- live_esc/kde_uas85uvc/kdecan_parse.py
+++ live_esc/kde_uas85uvc/kdecan_parse.py
@@ -116,6 +116,65 @@ class KdecanParser:
 
 class EscidParserTools:
     @staticmethod
+    def get_arm_dict(escid_dict):
+        esc11_df = escid_dict['esc11_df']
+        esc12_df = escid_dict['esc12_df']
+        esc13_df = escid_dict['esc13_df']
+        esc14_df = escid_dict['esc14_df']
+        esc15_df = escid_dict['esc15_df']
+        esc16_df = escid_dict['esc16_df']
+        esc17_df = escid_dict['esc17_df']
+        esc18_df = escid_dict['esc18_df']
+
+        esc11_power = esc11_df[KdecanParser.col_voltage].values * esc11_df[
+            KdecanParser.col_current].values
+        esc12_power = esc12_df[KdecanParser.col_voltage].values * esc12_df[
+            KdecanParser.col_current].values
+        esc13_power = esc13_df[KdecanParser.col_voltage].values * esc13_df[
+            KdecanParser.col_current].values
+        esc14_power = esc14_df[KdecanParser.col_voltage].values * esc14_df[
+            KdecanParser.col_current].values
+        esc15_power = esc15_df[KdecanParser.col_voltage].values * esc15_df[
+            KdecanParser.col_current].values
+        esc16_power = esc16_df[KdecanParser.col_voltage].values * esc16_df[
+            KdecanParser.col_current].values
+        esc17_power = esc17_df[KdecanParser.col_voltage].values * esc17_df[
+            KdecanParser.col_current].values
+        esc18_power = esc18_df[KdecanParser.col_voltage].values * esc18_df[
+            KdecanParser.col_current].values
+
+        arm1_power = esc11_power + esc16_power
+        arm2_power = esc12_power + esc15_power
+        arm3_power = esc13_power + esc18_power
+        arm4_power = esc14_power + esc17_power
+
+        arm1_eta_angvel = esc11_df[KdecanParser.col_rpm].values / esc16_df[
+            KdecanParser.col_rpm].values
+        arm2_eta_angvel = esc12_df[KdecanParser.col_rpm].values / esc15_df[
+            KdecanParser.col_rpm].values
+        arm3_eta_angvel = esc13_df[KdecanParser.col_rpm].values / esc18_df[
+            KdecanParser.col_rpm].values
+        arm4_eta_angvel = esc14_df[KdecanParser.col_rpm].values / esc17_df[
+            KdecanParser.col_rpm].values
+
+        arm1_eta_throttle = esc11_df[KdecanParser.col_inthtl].values / esc16_df[
+            KdecanParser.col_inthtl].values
+        arm2_eta_throttle = esc12_df[KdecanParser.col_inthtl].values / esc15_df[
+            KdecanParser.col_inthtl].values
+        arm3_eta_throttle = esc13_df[KdecanParser.col_inthtl].values / esc18_df[
+            KdecanParser.col_inthtl].values
+        arm4_eta_throttle = esc14_df[KdecanParser.col_inthtl].values / esc17_df[
+            KdecanParser.col_inthtl].values
+
+        arm_dict = {
+            'arm1': [arm1_power, arm1_eta_angvel, arm1_eta_throttle],
+            'arm2': [arm2_power, arm2_eta_angvel, arm2_eta_throttle],
+            'arm3': [arm3_power, arm3_eta_angvel, arm3_eta_throttle],
+            'arm4': [arm4_power, arm4_eta_angvel, arm4_eta_throttle],
+        }
+        return arm_dict
+
+    @staticmethod
     def resample(escid_dict, time_secs, max_delta):
         if DataframeTools.check_time_difference(escid_dict, max_delta):
             # time_secs = DataframeTools.shortest_time_secs(escid_dict)
